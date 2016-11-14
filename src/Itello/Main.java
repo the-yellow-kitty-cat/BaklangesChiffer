8943a6c76737f8b858b416102348b6b3327ddb26
the-yellow-kitty-cat <the.yellow.kitty.cat@gmail.com>
2016-11-14 17:17:31 +0100

    Created by COfver

--- src/Itello/Main.java
+++ src/Itello/Main.java
@@ -8,18 +8,14 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a string");
         String b = in.nextLine();
-        if (b.equals("")) {
-            System.out.println(b);
-        } else {
-            String encryptionToString = getBackwardsEncryption(b);
-            System.out.println(encryptionToString);
-        }
+        String encryptionToString = getBackwardsEncryption(b);
+        System.out.println(encryptionToString);
     }
 
     private static String getBackwardsEncryption(String b) {
         StringBuilder encryptedString = new StringBuilder();
         for (int currentLetter = b.length() - 1; currentLetter >= 0; currentLetter--) {
-            char currentSymbol=b.charAt(currentLetter);
+            char currentSymbol = b.charAt(currentLetter);
             encryptedString.append(currentSymbol);
         }
         return encryptedString.toString();
